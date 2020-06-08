8106ce70f3ada1dd408c1b4b7d559cd56e84b0f2
Glitch (fcc-pato-api-project--timestamp-microservice) <none>
2020-06-08 01:59:48 +0000

    🚥🏛 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+app.get("/api/timestamp/:dateString?", (req, res) => {
+  let dateString = req.params.dateString;
+  let date;
+
+  if (!dateString) {
+    date = new Date();
+  } else {
+    date = new Date(dateString);
+  }
+
+  if (isNaN(date.getDate())) {
+    date = new Date(parseInt(dateString));
+    if (isNaN(date.getDate())) {
+      res.json({ error: "Invalid Date" });
+    }
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
